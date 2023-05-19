65da96c1dfa180794b3919065ce995be39db45d7
Miguel <kevin.miguel@all-win.software>
2023-05-19 13:59:28 -0700

    Define Project Structure

--- app/src/main/java/com/example/rick_and_morty_android_networking_example_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/rick_and_morty_android_networking_example_app/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.rick_and_morty_android_networking_example_app
+package com.example.rick_and_morty_android_networking_example_app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rick_and_morty_android_networking_example_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
